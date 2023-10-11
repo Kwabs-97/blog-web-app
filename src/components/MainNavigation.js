33ed52f5cc0f87c18adadcf996ec53b8e543d53e
Kwabs-97 <khobewayne@gmail.com>
2023-10-11 07:13:09 +0000

    set up main nav and added google fonts

--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -1,11 +1,21 @@
-import React from 'react'
+/** @format */
 
+import React from "react";
+import styles from "../styles/MainNavigation.module.css";
+import { NavLink } from "react-router-dom";
 function MainNavigation() {
   return (
-    <div>
-      
-    </div>
-  )
+    <nav>
+      <ul>
+        <li>
+          <NavLink to="/">All Blogs</NavLink>
+        </li>
+        <li>
+          <NavLink to="/new">New Blog</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
-export default MainNavigation
+export default MainNavigation;
