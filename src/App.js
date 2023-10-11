aba105bb4f053dca8827a219eb300b064e88040c
Kwabs-97 <khobewayne@gmail.com>
2023-10-11 07:49:03 +0000

    styled index styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 /** @format */
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Blogs, { loader as blogsLoader} from './pages/Blogs'
 import Root from "./components/Root";
 const router = createBrowserRouter([{
   path: '/',
   element: <Root />,
   children: [
-
+    {
+      index: true,
+      element: <Blogs />,
+      loader: blogsLoader
+    }
   ]
 }])
 function App() {
--- src/pages/Blogs.js
+++ src/pages/Blogs.js
@@ -2,11 +2,12 @@
 
 import React from "react";
 import BlogList from "../components/BlogList";
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, json } from "react-router-dom";
 
 function Events() {
   const data = useLoaderData();
-  console.log(data);
+  const blogs = data.b1
+  console.log(blogs);
   return (
     <div>
       <BlogList />
@@ -16,9 +17,10 @@ function Events() {
 
 export default Events;
 
-async function loader() {
+export async function loader() {
   const response = await fetch("https://blogdata-392a9-default-rtdb.firebaseio.com/blogs.json");
   if (!response.ok) {
     throw json({ message: "Could not fetch blog data" }, { status: 500 });
   }
+  return response;
 }
