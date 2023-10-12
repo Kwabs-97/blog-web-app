bdd9c3aa74aeb6e18ce3d250cc4ea31635caae8f
Kwabs-97 <khobewayne@gmail.com>
2023-10-12 19:27:37 +0000

    add collection to firebase

--- src/components/BlogForm.js
+++ src/components/BlogForm.js
@@ -1,20 +1,55 @@
 /** @format */
 
 import React from "react";
-import { useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import styles from "../styles/BlogForm.module.css";
-
+import { useState } from "react";
+import { addDoc, collection } from "firebase/firestore";
+import { db } from "../config/firebase";
+import { redirect } from "react-router-dom";
 function BlogForm({ method, blog }) {
+  //input states
+  const [title, setTitle] = useState("");
+  const [category, setCategory] = useState("");
+  const [date, setDate] = useState("");
+  const [image, setImage] = useState("");
+  const [description, setDescription] = useState("");
+
+  const blogsCollectionRef = collection(db, "blogs");
+
   const navigate = useNavigate();
   function cancelHandler() {
     navigate("..");
   }
 
+  async function submitHandler(e) {
+    e.preventDefault();
+    try {
+      await addDoc(blogsCollectionRef, {
+        title: title,
+        category: category,
+        date: date,
+        image: image,
+        description: description,
+      });
+    } catch (error) {
+      throw new Error(error);
+    }
+
+    redirect("/");
+  }
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={submitHandler}>
       <p>
         <label htmlFor="title">Title</label>
-        <input id="title" type="text" name="title" required  />
+        <input
+          id="title"
+          type="text"
+          name="title"
+          required
+          onChange={(e) => setTitle(e.target.value)}
+        />
       </p>
       <p>
         <label htmlFor="category">Category</label>
@@ -23,16 +58,30 @@ function BlogForm({ method, blog }) {
           type="text"
           name="category"
           required
-         
+          onChange={(e) => setCategory(e.target.value)}
         />
       </p>
       <p>
         <label htmlFor="image">Image</label>
-        <input id="image" type="url" name="image" required  />
+        <input
+          id="image"
+          type="url"
+          name="image"
+          required
+          onChange={(e) => {
+            setImage(e.target.value);
+          }}
+        />
       </p>
       <p>
         <label htmlFor="date">Date</label>
-        <input id="date" type="date" name="date" required />
+        <input
+          id="date"
+          type="date"
+          name="date"
+          required
+          onChange={(e) => setDate(e.target.value)}
+        />
       </p>
       <p>
         <label htmlFor="description">Description</label>
@@ -41,7 +90,7 @@ function BlogForm({ method, blog }) {
           name="description"
           rows="5"
           required
-         
+          onChange={(e) => setDescription(e.target.value)}
         />
       </p>
       <div className={styles.actions}>
@@ -55,4 +104,3 @@ function BlogForm({ method, blog }) {
 }
 
 export default BlogForm;
-
